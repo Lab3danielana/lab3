4e5d6a128ef64320e13edf3967f6fe3712545fb1
Daniel Cubides <niroxdf@gmail.com>
2015-06-18 21:09:37 -0500

    -modify class Product

--- src/Product.java
+++ src/Product.java
@@ -2,4 +2,47 @@
  * Created by Nirox on 6/18/2015.
  */
 public class Product {
+    private int productId;
+    private String category;
+    private String description;
+    private long price;
+
+    public Product(int productId, String category, String description, long price) {
+        this.productId = productId;
+        this.category = category;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
 }
