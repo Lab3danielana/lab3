096b595914990bde0870946018d80dd199eef614
Ana Torres <aitorresp@unal.edu.co>
2015-06-19 07:30:55 -0500

    User

--- src/User.java
+++ src/User.java
@@ -1,5 +1,92 @@
-/**
- * Created by Nirox on 6/18/2015.
- */
+
 public class User {
+
+    private String name;
+    private String email;
+    private String userName;
+    private String password;
+    private String loginStatus;
+    
+    
+    public boolean verifyLogin(String name, String password){
+        if(name==getName() && password==getPassword()){
+            return true;
+        }
+        
+        if(name==getUserName() && password==getPassword()){
+            return true;
+        }
+        
+        return false;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the loginStatus
+     */
+    public String getLoginStatus() {
+        return loginStatus;
+    }
+
+    /**
+     * @param loginStatus the loginStatus to set
+     */
+    public void setLoginStatus(String loginStatus) {
+        this.loginStatus = loginStatus;
+    }
 }
